5d60700e6f61476814cc30c6af02b6b9685948fe
Tazy <tazy30@gmail.com>
2019-04-24 17:11:00 +0200

    Viewing Burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import Classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  //Extracting all the keys of the ingredients object into an Array
+  let transformedIngredeints = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      //Looping on every ingredientKey
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        //Looping as many times as the value of each ingredientKey
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    }
+  );
+
   return (
     <div className={Classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredeints}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      bacon: 3,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </Fragment>
     );
