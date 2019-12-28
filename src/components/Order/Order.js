a950cc84db1d6831e780e38c88881494865fc45c
Mohamed Eltazy <tazy@macusers-iMac.local>
2019-12-28 12:00:15 +0200

    fetching orders from the DB and listing them on the UI

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Classes from './Order.css';
 
-const order = props => (
-  <div className={Classes.Order}>
-    <p>Ingredients: Salad(1)</p>
-    <p>
-      Price: <strong>USD 4.3</strong>
-    </p>
-  </div>
-);
+const order = props => {
+  //Converting ingredients from an object to an array
+  const ingredientsArr = [];
+  for (let key in props.ingredients) {
+    ingredientsArr.push({ name: key, amount: props.ingredients[key] });
+  }
+
+  const IngredientOutput = ingredientsArr.map(ig => {
+    return (
+      <span
+        key={ig.name}
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '4px 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className={Classes.Order}>
+      <p>Ingredients: {IngredientOutput}</p>
+      <p>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import Spinner from '../../components/Spinner/Spinner';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+    errorLoadingOrders: false
+  };
+
+  componentDidMount() {
+    axios
+      .get('./orders.json')
+      .then(res => {
+        this.setState({ loading: false });
+        //const valuesArr = Object.values(res.data);
+        const fetchedOrdersArr = [];
+        for (let key in res.data) {
+          fetchedOrdersArr.push({ ...res.data[key], id: key });
+        }
+        console.log(fetchedOrdersArr);
+        this.setState({ orders: fetchedOrdersArr });
+      })
+      .catch(err => {
+        this.setState({ loading: false, errorLoadingOrders: true });
+        console.log(err);
+      });
+  }
+
   render() {
-    return (
+    let ordersBody = (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          );
+        })}
       </div>
+    );
+
+    return this.state.loading ? (
+      <Spinner />
+    ) : this.state.errorLoadingOrders ? (
+      <h1 style={{ textAlign: 'center', color: 'red' }}>
+        Network Error. Please Check Your Internet Connection
+      </h1>
+    ) : (
+      ordersBody
     );
   }
 }
